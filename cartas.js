baa34dc6c4ef1b73e7421e726e51a17c5f57c27a
icaro1007 <113843445+icaro1007@users.noreply.github.com>
2026-06-17 07:59:27 -0300

    Add files via upload

---
+++ cartas.js
@@ -0,0 +1,40 @@
+const bancoDeCartas = [
+            // --- CARTAS DO LOTE 1 ---
+            { id: "guerreiro", nome: "Guerreiro", vida: 5, dano: 1, img: "guerreiro.png", qtd: 4 },
+            { id: "barbaro", nome: "Bárbaro", vida: 4, dano: 2, img: "barbaro.png", qtd: 3 },
+            { id: "goblin", nome: "Goblin", vida: 4, dano: 2, img: "goblin.png", qtd: 3 },
+            { id: "ladrao", nome: "Ladrão", vida: 5, dano: 0, img: "ladrão.png", qtd: 2 },
+            { id: "necromante", nome: "Necromante", vida: 5, dano: 1, img: "necromante.png", qtd: 2 },
+            { id: "ork", nome: "Ork", vida: 3, dano: 3, img: "ork.png", qtd: 2 },
+            { id: "triogoblin", nome: "Trio de Goblin", vida: 6, dano: 4, img: "trio de goblim.png", qtd: 2 },
+            { id: "unidao", nome: "Unidão", vida: 4, dano: 1, img: "unidão.png", qtd: 2 },
+            { id: "cavaleiro", nome: "Cavaleiro das Trevas", vida: 3, dano: 1, img: "cavaleiro das trevas.png", qtd: 2 },
+            { id: "curandeiro", nome: "Curandeiro", vida: 4, dano: 0, img: "curandeiro.png", qtd: 2 },
+            { id: "ctrlc", nome: "Ctrl C", vida: 1, dano: 1, img: "ctrl c.png", qtd: 2 },
+            { id: "ctrlv", nome: "Ctrl V", vida: 1, dano: 1, img: "ctrl v.png", qtd: 2 },
+
+            // --- CARTAS DO LOTE 2 ---
+            { id: "arqueiro", nome: "Arqueiro", vida: 3, dano: 2, img: "arqueiro.png", qtd: 3 },
+            { id: "bruxo", nome: "Bruxo", vida: 4, dano: 0, img: "bruxo.png", qtd: 4 }, 
+            { id: "barril", nome: "Barril", vida: 2, dano: 0, img: "barril.png", qtd: 3 },
+            { id: "barrilbarbaro", nome: "Barril de Bárbaro", vida: 2, dano: 2, img: "barril de barbaro.png", qtd: 2 },
+            { id: "barrilgoblin", nome: "Barril de Goblin", vida: 3, dano: 0.75, img: "barril de goblim.png", qtd: 2 },
+            { id: "besta", nome: "Besta", vida: 0, dano: 0, img: "besta.png", qtd: 2 },
+            { id: "veluz", nome: "Veluz", vida: 0, dano: 0, img: "veluz.png", qtd: 2 }, 
+            { id: "adiv", nome: "Adiv", vida: 0, dano: 0, img: "adiv.png", qtd: 2 }, 
+            { id: "recuperida", nome: "Recuperida", vida: 0, dano: 0, img: "recuperida.png", qtd: 2 }, 
+            { id: "pocaotraicao", nome: "Poção da Traição", vida: 0, dano: 0, img: "poção da traição.png", qtd: 2 },
+            
+            // --- CARTAS DO LOTE 3 ---
+            { id: "pocaogelo", nome: "Poção de Gelo", vida: 0, dano: 0, img: "poção de gelo.png", qtd: 2 },
+            { id: "bumerskeleton", nome: "Bumerskeleton", vida: 3, dano: 1, img: "bumerskeleton.png", qtd: 2 }, 
+            { id: "mensageiro", nome: "Mensageiro", vida: 2, dano: 3, img: "mensageiro.png", qtd: 2 },
+            { id: "cavalotroia", nome: "Cavalo de Tróia", vida: 1, dano: 0, img: "cavalo de troia.png", qtd: 2 }, 
+            { id: "criador", nome: "Criador", vida: 2, dano: 2, img: "criador.png", qtd: 2 },
+            { id: "escudo_item", nome: "Escudo", vida: 0, dano: 0, img: "escudo.png", qtd: 2 },
+            { id: "fogueira", nome: "Fogueira", vida: 0, dano: 0, img: "fogueira.png", qtd: 2 },
+            { id: "separado", nome: "Separado", vida: 3, dano: 2, img: "separado.png", qtd: 2 },
+            { id: "separado2", nome: "Separadois", vida: 3, dano: 2, img: "separado 2.png", qtd: 2 }, 
+            { id: "viajante", nome: "Viajante do Tempo", vida: 4, dano: 1, img: "viajante do tempo.png", qtd: 2 },
+            { id: "incendiario", nome: "Incendiário", vida: 4, dano: 2, img: "incendiário.png", qtd: 3 }
+        ];
